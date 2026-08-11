e8f97c0067bc2f24d2304f33306dc9d10c73f79f
ingridcernauski <ingridcernauski@gmail.com>
2026-08-10 21:29:45 -0300

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/home/home";
 import Metodologia from "./pages/metodologia/Metodologia";
@@ -41,6 +41,9 @@ function App() {
         />
 
       </Routes>
+
+            <Analytics />
+
 
     </BrowserRouter>
   );
